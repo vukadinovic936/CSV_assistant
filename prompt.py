8b30559802597071b8e2c4f67682446d6605e240
vukadinovic936 <milosvuk99@gmail.com>
2023-05-21 12:51:11 -0700

    demo

--- prompt.py
+++ prompt.py
@@ -4,8 +4,15 @@ from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
 import pandas as pd
 
+# load api key
 with open('openai_key.txt') as f:
     key = f.readlines()
 os.environ['OPENAI_API_KEY'] = key[0]
-agent = create_csv_agent(OpenAI(temperature=0), 'titanic.csv', verbose=True)
-agent.run("Fit a linear regression with Age column as a predictor and Fare column as a criterion and tell me what is the p-value, and thecorrelation coefficient")
+
+# load csv file (ex: titanic.csv)
+csv_path = input("Enter the path of your csv file \n")
+agent = create_csv_agent(OpenAI(temperature=0), csv_path , verbose=True)
+
+# ask the question ex: What is the average fare?
+query = input("Enter your query\n")
+agent.run(query)
