c826d9f3b05651c5ecffe1f69a43f1a148f89762
vukadinovic936 <milosvuk99@gmail.com>
2023-05-21 11:38:56 -0700

    init

--- prompt.py
+++ prompt.py
@@ -4,7 +4,8 @@ from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
 import pandas as pd
 
-
-os.environ['OPENAI_API_KEY'] = "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"
+with open('openai_key.txt') as f:
+    key = f.readlines()
+os.environ['OPENAI_API_KEY'] = key[0]
 agent = create_csv_agent(OpenAI(temperature=0), 'titanic.csv', verbose=True)
 agent.run("Fit a linear regression with Age column as a predictor and Fare column as a criterion and tell me what is the p-value, and thecorrelation coefficient")
